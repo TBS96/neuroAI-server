57db164d561a00c1a5eff3f2c21ee47342971359
TBS96 <9tbs6@proton.me>
2025-06-12 22:57:41 +0530

    fix: added STATIC_ROOT for collectstatic
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- Mentalhealth/settings.py
+++ Mentalhealth/settings.py
@@ -201,6 +201,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
