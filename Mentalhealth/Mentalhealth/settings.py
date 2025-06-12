ce6b847840cf4444d6655592752290b42576a450
TBS96 <9tbs6@proton.me>
2025-06-12 19:52:08 +0530

    migration from sqlite3 to postgres
    - Update environment configuration,
    - Migrate uncommited model for chathistory (0012_chathistory.py)
    - Migrate database settings to postgres,
    - Register new models in admin
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- Mentalhealth/Mentalhealth/settings.py
+++ Mentalhealth/Mentalhealth/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY')
 
@@ -105,12 +106,20 @@ WSGI_APPLICATION = 'Mentalhealth.wsgi.application'
 
 AUTH_USER_MODEL = "NeuroAI.RegisterUser"
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
+
+# print(DATABASES)
 
 
 # Password validation
--- Mentalhealth/NeuroAI/admin.py
+++ Mentalhealth/NeuroAI/admin.py
@@ -1,10 +1,26 @@
-from django.contrib import admin
-from .models import Questions,Disorder, DisorderSave
+# from django.contrib import admin
+# from .models import Questions,Disorder, DisorderSave
 
-# Register your models here.
+# # Register your models here.
+# admin.site.register(Questions)
+# admin.site.register(Disorder)
+# admin.site.register(DisorderSave)
+
+
+
+from django.contrib import admin
+from .models import (
+    Questions,
+    Disorder,
+    DisorderSave,
+    Response,
+    RegisterUser,
+    ChatHistory,
+)
+
 admin.site.register(Questions)
 admin.site.register(Disorder)
 admin.site.register(DisorderSave)
-
-
-
+admin.site.register(Response)
+admin.site.register(RegisterUser)
+admin.site.register(ChatHistory)
